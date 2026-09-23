d8a0288309b173d7c48487a6a29421d8e215bc95
Nathan K <nathankidd@hey.com>
2026-09-23 19:21:25 +0100

    Quick Git Push

---
+++ templates/flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.mine.url = "github:n-at-han-k/flake.nix";
+
+  outputs = { mine, ... }:
+    let
+      lib = mine.lib.x86_64-linux;
+      gems = lib.buildGemset { name = "ratalada"; src = ./.; };
+    in
+    {
+      devShells.x86_64-linux.default = lib.mkRubyViteShell {
+        buildInputs = [ gems gems.wrappedRuby ];
+      };
+    };
+}
