f0d0da939bcece924aae861bdc2d8b218f6e01f8
Nathan K <nathankidd@hey.com>
2026-09-23 19:26:46 +0100

    quick push

---
+++ templates/ratalada/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.mine.url = "github:n-at-han-k/flake.nix";
+
+  outputs = { mine, ... }:
+    let
+      lib = mine.lib.x86_64-linux;
+      gems = lib.buildGemset {
+        name = "ratalada";
+        src = ./.;
+      };
+    in
+    {
+      devShells.x86_64-linux.default = lib.mkRubyViteShell {
+        buildInputs = [
+          gems
+          gems.wrappedRuby
+        ];
+      };
+    };
+}
---
+++ templates/ratalada/gemset.nix
@@ -0,0 +1,12 @@
+{
+  json = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1pqrpn0wkxvhcrwm4i0z9ailjj7if45zkkih0hij2k1q25xpwvcf";
+      type = "gem";
+    };
+    version = "3.0.2";
+  };
+}
