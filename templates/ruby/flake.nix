2f158dbf929f1b9e1c58bb206dee2c99c6ae166d
Nathan K <nathankidd@hey.com>
2026-09-26 14:47:27 +0100

    Quick Git Push

--- templates/ruby/flake.nix
+++ templates/ruby/flake.nix
@@ -1,14 +1,22 @@
 {
   inputs.mine.url = "github:n-at-han-k/flake.nix";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { mine, ... }:
-    let
-      lib = mine.lib.x86_64-linux;
-      gems = lib.buildGemset { name = "ratalada"; src = ./.; };
-    in
-    {
-      devShells.x86_64-linux.default = lib.mkRubyShell {
-        buildInputs = [ gems gems.wrappedRuby ];
-      };
-    };
+  outputs = { mine, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = mine.lib.${system};
+        gems = lib.buildGemset { name = "ratalada"; src = ./.; };
+      in
+      {
+        devShells.default = lib.mkRubyShell {
+          buildInputs = with pkgs; [
+            gems
+            gems.wrappedRuby
+          ];
+        };
+      }
+    );
 }
